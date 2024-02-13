a48c5915b1cac6a7772b79fffb15c94cbe013ab6
Emre Can Akyol <emrecanakyol@Emre-MacBook-Pro.local>
2024-02-13 21:05:23 +0300

    Project complated.

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Emre Can Akyol on 13.02.2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
